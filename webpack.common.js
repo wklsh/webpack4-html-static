14153fa5c424e8ff60b9f3976fd183586705ebb0
Shawn Lim Wee Kiang <wklsh@hotmail.sg>
2018-12-22 19:06:30 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -4,20 +4,16 @@
 //// -----------------------------------------------------------------
 //// ** DO NOT INCLUDE DEV / PROD SPECIFIC SETTINGS IN HERE ***
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const fs = require("fs");
 const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-const HtmlWebpackImportStaticPages = require("html-webpack-import-static-pages");
+const WebpackBar = require("webpackbar");
+const PreloadWebpackPlugin = require("preload-webpack-plugin");
 
 module.exports = {
 	entry: {
-		app: path.resolve(__dirname, "src/js/index.js"),
-		landing: path.resolve(__dirname, "src/js/views/landing/page-landing.js")
+		app: path.resolve(__dirname, "src/app/index.js")
 	},
-
-	devtool: "cheap-module-source-map",
 
 	module: {
 		rules: [
@@ -41,6 +37,32 @@ module.exports = {
 						}
 					}
 				]
+			},
+
+			{
+				test: /\.(mov|mp4|mp3)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							publicPath: path.resolve(__dirname, "/"),
+							outputPath: path.resolve(__dirname, "/"),
+							context: path.resolve(__dirname, "src"),
+							name: process.env.NODE_ENV == "development" ? "[path][name].[ext]" : "assets/[sha512:hash:base64:7].[ext]"
+						}
+					}
+				]
+			},
+
+			{
+				test: /\.(woff|woff2|eot|ttf)$/i,
+				loader: "file-loader",
+				options: {
+					publicPath: path.resolve(__dirname, "/"),
+					outputPath: path.resolve(__dirname, "/"),
+					context: path.resolve(__dirname, "src"),
+					name: process.env.NODE_ENV == "development" ? "[path][name].[ext]" : "css/type/[sha512:hash:base64:7].[ext]"
+				}
 			}
 		]
 	},
@@ -48,23 +70,43 @@ module.exports = {
 	resolve: {
 		extensions: [".css", ".scss", ".js", ".jsx"],
 		alias: {
-			StyleAlias: path.resolve(__dirname, "src/scss"),
-			JsAlias: path.resolve(__dirname, "src/js")
+			AppAlias: path.resolve(__dirname, "src/app"),
+			BaseAlias: path.resolve(__dirname, "src/app/base"),
+			StyleAlias: path.resolve(__dirname, "src/app/base/styles"),
+			ComponentsAlias: path.resolve(__dirname, "src/app/components"),
+			ReduxAlias: path.resolve(__dirname, "src/app/redux"),
+			ImgAlias: path.resolve(__dirname, "src/img"),
+			AudioAlias: path.resolve(__dirname, "src/audio")
 		}
 	},
 
 	plugins: [
-		new FriendlyErrorsWebpackPlugin({
-			compilationSuccessInfo: {
-				messages: ["Application is running.", "http://localhost:3000"]
-			}
+		new WebpackBar(),
+
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html",
+			inject: "body",
+			chunksSortMode: "none"
 		}),
 
-		new HtmlWebpackImportStaticPages({
-			// blacklist: ["page2"],
-			chunkAssign: {
-				index: ["app", "landing"]
-			}
+		new PreloadWebpackPlugin({
+			// Preload attr type to attach
+			rel: "preload",
+
+			// Read all assets first
+			include: "allAssets",
+
+			// Assign [as] attribute to special cases, else revert to script
+			as(entry) {
+				if (/\.(woff|woff2|eot|ttf)$/.test(entry)) return "font";
+				if (/\.(gif|png|jpe?g|svg)$/.test(entry)) return "image";
+				if (/\.(mov|mp4|mp3|webm|webp)$/.test(entry)) return "video";
+				return "script";
+			},
+
+			// Remove from preload list - since theres already a preloader
+			fileBlacklist: [/\.(gif|png|jpe?g|svg)/]
 		}),
 
 		new CopyWebpackPlugin([
