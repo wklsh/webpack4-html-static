b57449e39af474696378039813941cc5ccf41279
Shawn Lim Wee Kiang <wklsh@hotmail.sg>
2019-11-23 02:22:27 +0800

    fix: If case typo for Offline plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
-import * as OfflinePluginRuntime from "offline-plugin/runtime";
-
 import "../styles/index.scss";
 
-if (("process.env.NODE_ENV:", process.env.NODE_ENV === "production")) {
+if (process.env.NODE_ENV === "production") {
 	console.log("Installing Offline-plugin");
-	OfflinePluginRuntime.install();
+	require("offline-plugin/runtime").install();
 }
-
